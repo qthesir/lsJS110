927f7ac3c0a4d8afc17f2df8f37527a266d106dc
q_the_sir <quinlan.anderson@gmail.com>
2025-03-23 13:44:00 -0400

    practice problems complete

--- lesson_1/practiceProblems.js
+++ lesson_1/practiceProblems.js
@@ -135,21 +135,190 @@ Should have mentioned that the method is returning an array. This is implied, bu
 good. 
 */
 
-// Practice Problem 8 
+// Problem 8
 
-// Take a look at the following array: 
+// Take a look at the following array:
 
 let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "Bambam"];
 
-// Write a program that uses this array to create an object where the names are the keys and 
+// Write a program that uses this array to create an object where the names are the keys and
 // the values are the positions in the array:
 
-{ Fred: 0, Barney: 1, Wilma: 2, Betty: 3, Pebbles: 4, Bambam: 5 }
+// { Fred: 0, Barney: 1, Wilma: 2, Betty: 3, Pebbles: 4, Bambam: 5 }
 
 /*
 The goal here is to create a new object; not to modify the existing array. Therefore, if we are not 
-performing any sort of transformation on the array, nor selecting, this feels like
+performing any sort of transformation on the array, nor selecting, only iterating, this feels like
 a good use case for the native JavaScript array iterator, .forEach. 
+
+Success! 
+*/
+
+let flintstonesObj = {};
+
+flintstones.forEach((ele, idx) => {
+  flintstonesObj[ele] = idx;
+});
+
+console.log(flintstonesObj); // { Fred: 0, Barney: 1, Wilma: 2, Betty: 3, Pebbles: 4, Bambam: 5 }
+
+// Problem 9
+
+// Add up all of the ages from the Munster family object:
+
+let ages = {
+  Herman: 32,
+  Lily: 30,
+  Grandpa: 5843,
+  Eddie: 10,
+  Marilyn: 22,
+  Spot: 237,
+};
+
+let totalAge = 0;
+
+console.log(Object.values(ages));
+Object.values(ages).forEach((age) => (totalAge += age));
+
+console.log(totalAge);
+
+// One note here: I initially set totalAge to just totalAge, not equal to 0. This was giving me an
+// error where the value of totalAge was NaN, which makes sense, because a variable is initialized
+// to undefined if not explicitly given a value, and undefined + 32 or any number is NaN, and NaN
+// plus any number is NaN, which is why the return value was NaN.
+
+// Another possible solution:
+
+console.log(Object.values(ages).reduce((acc, val) => acc + val, 0));
+
+// The advantage of using the reduce method is that you don't have to initialize another variable
+// in order to solve the problem. You can solve it in one line.
+
+// Problem 10
+
+// Pick out the minimum age from our current Munster family object:
+
+let ages2 = {
+  Herman: 32,
+  Lily: 30,
+  Grandpa: 5843,
+  Eddie: 10,
+  Marilyn: 22,
+  Spot: 237,
+};
+
+/*
+In this case, we'd be using a selection method, which would be the filter method, to find out 
+which age is the lowest. There probably is also a built in array iterator "min," perhaps from the Math
+library, but lets try this first. 
+
+Actually, on second thought, this isn't really a selection method. You are not selecting something 
+that defines a specific criteria. You're really iterating through the array and looking for a specific 
+value, so forEach seems more appropriate.
+
+The math.min works with the spreader syntax. I'm trying to figure out how exactly I might use the filter
+method to solve this problem, but I'm coming up a bit stumped. How would you have the value return 
+true for the minimum value? Because you're not returning an array, you're returning a single value,
+and the conditional in forEach is going to evaluate to true multiple times. 
+
+You could potentially use reduce though. 
+*/
+
+let minNum = Infinity;
+Object.values(ages2).forEach((num) => {
+  if (num < minNum) {
+    minNum = num;
+  }
+});
+console.log(minNum);
+console.log(Math.min(...Object.values(ages2)));
+
+console.log(
+  Object.values(ages2).reduce((acc, cv) => {
+    if (cv < acc) {
+      return cv;
+    }
+    return acc;
+  }, Infinity)
+);
+
+// Problem 11
+
+// Create an object that expresses the frequency with which each letter occurs in this string:
+
+let statement = "The Flintstones Rock";
+
+// The output will look something like the following:
+
+// { T: 1, h: 1, e: 2, F: 1, l: 1, ... }
+
+/*
+Lets think about this for a second. First step is probably going to be to split the string into an 
+array. Then, I can iterate through that array, and use the current letter as an object key. For each
+letter, I can add 1 to the array value, hopefully with += 1. 
+
+
+Implied Assumptions
+- Capital letters are separate from lowercase, and counted differently. 
+- Spaces are not counted. Skip spaces
+*/
+
+let letterFrequency = {};
+statement.split("").forEach((letter) => {
+  if (letter !== " ") {
+    if (letterFrequency[letter] === undefined) {
+      letterFrequency[letter] = 1;
+    } else {
+      letterFrequency[letter] += 1;
+    }
+  }
+});
+console.log(letterFrequency);
+
+// Noticing a pattern with these forEach iterators. I could easily replicate this with an accumulator.
+// That is, the object {} would be the beginning of the accumulator.
+// 1 issue here is that each of the letters needs to be initialized to a number initially. The logic
+// is a little complicated and difficult to read here. Might have been better to potentially use a 
+// full for loop, for the sake of being able to use the continue statement.
+
+console.log(statement.split("").reduce((acc, cv) => {
+  if (cv !== " ") {
+    if(acc[cv] === undefined) {
+      acc[cv] = 1
+    } else {
+      acc[cv] += 1
+    }
+  }
+  return acc
+}, {}))
+
+// Couple of notes here. The LS solution is really elegant, see: 
+
+let charsInStatement = statement.split('').filter(char => char !== ' ');
+let result = {};
+
+charsInStatement.forEach(char => {
+  result[char] = result[char] || 0;
+  result[char] += 1;
+});
+
+console.log(result);
+
+/*
+Reflection:
+
+First off, I could have probably simplified getting rid of the white space by just using the filter 
+first, instead of looking for a fancy way to do it in a chain. Don't know how or why I missed this tbh.
+
+Using the "or" || logical operator and the short-circuiting behavior here was pretty slick, I think.
+So, if the result[char] is undefined, its going to be a falsy value, so the or statement will look at 
+the second value, which is 0, and evaluate to that instead, since that is the last value evaluated. 
+If there is a value already in result[char], it will be greater than 1 and therefore a truthy value,
+meaning that the result[char] will be evaluated and the result will not change. This is a clever,
+simple way to get around my nested if statements, which are kind of confusing to read. In this case,
+there literally are no if statements.
+
+Also, per the solution, good to note that this can be solved without transforming the string into an
+array. This can be done with only a for loop, iterating over each character of the string, checking 
+for spaces etc. 
 */ 
-
-
