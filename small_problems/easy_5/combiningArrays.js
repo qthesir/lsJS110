9d7d836a04380b4cab1a92d4b3012b4d68971d0e
q_the_sir <quinlan.anderson@gmail.com>
2025-05-08 14:55:28 -0400

    combining arrays problem

---
+++ small_problems/easy_5/combiningArrays.js
@@ -0,0 +1,26 @@
+/*
+Write a function that takes two arrays as arguments and returns an array containing the 
+union of the values from the two. There should be no duplication of values in the returned array, 
+even if there are duplicates in the original arrays. You may assume that both arguments will always 
+be arrays.
+
+*/ 
+
+// Example
+
+union([1, 3, 5], [3, 6, 9]);    // [1, 3, 5, 6, 9]
+
+/*
+PEDAC
+
+Problem
+
+Examples
+
+Data Structure
+
+Algorithm
+
+*/ 
+
+// Code With Intent
