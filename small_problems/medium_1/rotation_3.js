03ed8940152ebf525c68600c70c3e2d9139138da
q_the_sir <quinlan.anderson@gmail.com>
2025-09-14 15:53:30 -0400

    maximum rotation wip

--- small_problems/medium_1/rotation_3.js
+++ small_problems/medium_1/rotation_3.js
@@ -1,13 +1,44 @@
 /*
-Take the number 735291 and rotate it by one digit to the left, getting 352917. Next, keep the first digit fixed in place and rotate the remaining digits to get 329175. Keep the first two digits fixed in place and rotate again to get 321759. Keep the first three digits fixed in place and rotate again to get 321597. Finally, keep the first four digits fixed in place and rotate the final two digits to get 321579. The resulting number is called the maximum rotation of the original number.
+Take the number 735291 and rotate it by one digit to the left, getting 352917. Next, 
+keep the first digit fixed in place and rotate the remaining digits to get 329175. 
+Keep the first two digits fixed in place and rotate again to get 321759. 
+Keep the first three digits fixed in place and rotate again to get 321597. 
+Finally, keep the first four digits fixed in place and rotate the final two digits to get 321579. 
+The resulting number is called the maximum rotation of the original number.
 
-Write a function that takes an integer as an argument and returns the maximum rotation of that integer. You can (and probably should) use the rotateRightmostDigits function from the previous exercise.
+Write a function that takes an integer as an argument and returns the maximum rotation of that integer. 
+You can (and probably should) use the rotateRightmostDigits function from the previous exercise.
 */
 
 // Examples:
 
-maxRotation(735291);          // 321579
-maxRotation(3);               // 3
-maxRotation(35);              // 53
-maxRotation(105);             // 15 -- the leading zero gets dropped
-maxRotation(8703529146);      // 7321609845
+maxRotation(735291); // 321579
+maxRotation(3); // 3
+maxRotation(35); // 53
+maxRotation(105); // 15 -- the leading zero gets dropped
+maxRotation(8703529146); // 7321609845
+
+/*
+PEDAC
+
+Problem
+
+Examples / Test Cases
+
+Data Structures
+
+Algorithm
+
+*/
+
+// Code with intent
+
+/*
+
+Notes and reflections
+
+What IS a maximum rotation? Its kind of difficult to form a pattern of the input and the output. It seems almost
+shuffled. How can I make sense of this with my human mind? Or, perhaps, I'm not supposed to, and the maximum 
+rotation steps are what I should use? 
+
+*/
