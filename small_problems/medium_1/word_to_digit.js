a7dc3d25ff3340e49651ed04d4723919a588502f
q_the_sir <quinlan.anderson@gmail.com>
2025-09-24 22:26:32 -0400

    wip word_to_digit.js :

--- small_problems/medium_1/word_to_digit.js
+++ small_problems/medium_1/word_to_digit.js
@@ -1,10 +1,69 @@
 /*
-Write a function that takes a sentence string as an argument and returns that string with every occurrence 
-of a "number word" — 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' — 
-converted to its corresponding digit character.
+Write a function that takes a sentence string as an argument and returns that string with 
+every occurrence of a "number word" — 'zero', 'one', 'two', 'three', 'four', 'five', 
+'six', 'seven', 'eight', 'nine' — converted to its corresponding digit character.
 */
 
 // Example
 
 wordToDigit('Please call me at five five five one two three four. Thanks.');
 // "Please call me at 5 5 5 1 2 3 4. Thanks."
+
+/* 
+PEDAC
+
+Problem
+The problem is asking us to take a sentence, convert every word form of a number into its corresponding digit,
+and return the sentence to the caller.
+
+Examples / Test Cases
+Requirements
+- Accept a string as an argument
+- Convert each 'number word' to its corresponding digit. E.g., 'five' will become '5' in the output sentence.
+- The output sentence will be in the form of a string
+- We are only dealing with 'number words' less than 10. 
+
+Questions
+- What about dealing with typos? Suppose thats out of scope for this problem. 
+
+Data Structures
+Input: A string sentence
+Output: The input sentence with 'number words' converted to their respective digits
+Intermediate: It may be useful to split the sentence into individual words to identify each one. Hm... Well, the 
+words may have punctuation, if I use a space as a delimeter. I could filter out non-alphabetic characters from 
+the word in order to account for noise. Or, I could simply use the .includes() array method to identify whether
+its the word or not. But I'd have to iterate through each letter / number pairing in the list... The more I think
+about it, the more I think it makes sense to do a filter on only alphabetic characters. This is also a more
+accurate way of doing it, because the word "boner" technically includes one. Ha!
+
+Algorithm
+Mental Model
+The function will accept a sentence as an argument. It will split the sentence into individual words. Each 
+word will have its non-alphanumeric characters removed (to account for punctuation) and then checked to see 
+if it is a digit word. If it is a digit word, the word will be converted to its respective digit before being
+added to the output array. The output array will be concatenated back into a sentence separated by 
+spaces and returned to the caller.
+
+Step by Step
+Main Functions
+- Accept a sentence as an argument
+- Declare an output array 'output' and initialize it to an empty string
+- Split the argument into a word array delimited by spaces
+- For each word:
+  - Remove extra spaces and punctuation from the word, leaving only alphabetic characters
+  - Check to see if the word is a number word
+  - If it is a number word, replace it with its corresponding digit
+  - Add the word to the output array
+- Concatenate the output array back into a sentence, separating each word by a space
+- Return the sentence to the caller 
+
+Helper: Remove extra spaces and punctuation
+
+Helper: Check to see if the word is a number word
+- 
+
+
+*/
+
+// Code with intent 
+
